98740ddf1e3b3cef68db73db2c0a2ab5d434c372
Mark Roman <mroman8465@gmail.com>
2025-09-19 18:58:12 -0400

    Fixed all initial linting issues.

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -25,13 +25,13 @@ export const Button = ({
 }: ButtonProps) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
+      <button
+          type="button"
+          className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+          style={{ backgroundColor }}
+          {...props}
     >
-      {label}
-    </button>
+          {label}
+      </button>
   );
 };
